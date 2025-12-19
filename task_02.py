73b70744ad161628fea1b1803aad775a2d8756d5
Alen Chacko <the.alenchacko@gmail.com>
2025-12-19 21:03:39 +0530

    Task 02 completed

---
+++ task_02.py
@@ -0,0 +1,12 @@
+import math
+
+number=float(input("Enter a number: "))
+
+square_root = math.sqrt(number)
+natural_log = math.log(number)
+sine_value = math.sin(number)
+
+
+print(f"Square root of {number}: {square_root}")
+print(f"Natural logarithm of {number}: {natural_log}")
+print(f"Sine of {number} (in radians): {sine_value}")
